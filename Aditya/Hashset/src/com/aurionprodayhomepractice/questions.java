a704fe79a241072832361b2ec36ff0f1bf167a0c
AdityaDinde <125594020+AdityaD20@users.noreply.github.com>
2025-07-25 13:12:34 +0530

    Add files via upload

---
+++ Aditya/Hashset/src/com/aurionprodayhomepractice/questions.java
@@ -0,0 +1,28 @@
+package com.aurionprodayhomepractice;
+import java.util.HashSet;
+
+public class questions {
+
+	
+	
+	// Append Element to HashSet
+	// Write a Java program to append the specified element to the end of a hash set.
+  public static void main(String[] args) {
+        HashSet<String> set = new HashSet<>();
+
+        set.add("Apple");
+        set.add("Banana");
+
+        // Append (add) element
+        set.add("Orange");
+
+        System.out.println("HashSet after adding element: " + set);
+        
+        
+        
+        
+    }
+}
+
+	
+
